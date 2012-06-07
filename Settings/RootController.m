876ac2ca3c4293a381777046520b247706ce57a6
jverkoey <jverkoey@gmail.com>
2012-06-06 19:24:19 -0700

    Use a switch cell for airplane mode and listen to changes.

--- Settings/RootController.m
+++ Settings/RootController.m
@@ -12,6 +12,10 @@
 @property (nonatomic, readwrite, retain) NITableViewModel* model;
 @end
 
+typedef enum {
+  AirplaneModeSwitch,
+} FormIds;
+
 @implementation RootController
 
 @synthesize model;
@@ -21,9 +25,14 @@
   if (self) {
     self.title = @"Settings";
 
+    // K, we need to show a switch cell here.
     NSMutableArray* contents =
     [NSMutableArray arrayWithObjects:
-     [NITitleCellObject objectWithTitle:@"Airplane Mode" image:[UIImage imageNamed:@"Settings-Air"]],
+     [NISwitchFormElement switchElementWithID:AirplaneModeSwitch
+                                    labelText:@"Airplane Mode"
+                                        value:YES
+                              didChangeTarget:self
+                            didChangeSelector:@selector(didChangeAirplaneMode:)],
      nil];
     // NICellFactory here allows us to take advantage of the pre-built bindings between
     // Nimbus cells. This way we don't have to create our own factory until we absolutely need to.
@@ -40,6 +49,22 @@
   [super loadView];
 
   self.tableView.dataSource = self.model;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+  id object = [self.model objectAtIndexPath:indexPath];
+  if ([object isKindOfClass:[NIFormElement class]]) {
+    NIFormElement* formElement = object;
+    if (AirplaneModeSwitch == formElement.elementID) {
+      cell.imageView.image = [UIImage imageNamed:@"Settings-Air"];
+    }
+  }
+}
+
+#pragma mark - Form Elements
+
+- (void)didChangeAirplaneMode:(UISwitch *)element {
+  NSLog(@"%d", element.on);
 }
 
 @end
